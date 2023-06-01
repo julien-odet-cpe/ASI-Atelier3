17429789995f350780de421f7a773edf7346c02d
valen <valentinfoex@gmail.com>
2023-06-01 14:27:44 +0200

    hero

---
+++ HeroMicroService/src/main/java/com/hero/heromicroservice/HeroMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hero.heromicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroMicroServiceApplication.class, args);
+    }
+
+}
---
+++ HeroMicroService/src/test/java/com/hero/heromicroservice/HeroMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hero.heromicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
